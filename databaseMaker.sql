d4ad78d44ef82ef745b3166610fa9cfb5ecb5a8f
Dennis Li <li.dennis.beta@gmail.com>
2020-09-22 20:07:00 -0700

    Create databaseMaker.sql
    beginning of mysql database

---
+++ databaseMaker.sql
@@ -0,0 +1,23 @@
+drop database if exists bookhandler;
+create database bookhandler;
+/*
+Things that a book has:
+Database Entry Number
+Author/Co-Authors
+Illustator/Illustrators - can be set to none
+Title
+Length - can be set to unspecified
+Format - Hardcover,Paperback,Digital Kindle, Digital PDF
+ISBN - specific to each format of a book
+Language - Default English
+Publisher
+Date of Release
+Genre
+
+Expected Retail Price
+Copies Sold
+Total Price of Volumes sold
+
+Rating
+Number of Raters
+*/
