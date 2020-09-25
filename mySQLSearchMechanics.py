1f78bfa3bbd1146097d88ceaf2b65713b86e5eaa
Dennis Li <li.dennis.beta@gmail.com>
2020-09-25 01:57:33 -0700

    all but adv search

--- mySQLSearchMechanics.py
+++ mySQLSearchMechanics.py
@@ -33,7 +33,52 @@ WHERE personName LIKE %s
     res = cursor.fetchall();
     return res;
 
-#search by rating
-#search by title
 #search by publisher
-def findBookBy
+def findBookByPublisher(conn,publisher):
+    query = """
+        select generalInfo.dbNumber,title,lang,publisher,length,releaseDate,avgRating,price
+        from  generalInfo 
+        left join formats ON formats.dbNumber = generalInfo.dbNumber 
+        left join ratingsTotal on formats.versionNumber = ratingsTotal.versionNumber and ratingsTotal.dbNumber = formats.dbNumber
+        left join pricing on formats.versionNumber = pricing.versionNumber AND pricing.dbNumber = formats.dbNumber
+        WHERE publisher LIKE %s
+    """
+    cursor = conn.cursor(prepared = True);
+    cursor.execute(query,("%" + publisher + "%"));
+    res = cursor.fetchall();
+    return res; 
+
+#search by title
+def findBookByTitle(conn,title):
+    query = """
+        select generalInfo.dbNumber,title,lang,publisher,length,releaseDate,avgRating,price
+        from  generalInfo 
+        left join formats ON formats.dbNumber = generalInfo.dbNumber 
+        left join ratingsTotal on formats.versionNumber = ratingsTotal.versionNumber and ratingsTotal.dbNumber = formats.dbNumber
+        left join pricing on formats.versionNumber = pricing.versionNumber AND pricing.dbNumber = formats.dbNumber
+        WHERE title LIKE %s
+    """
+    cursor = conn.cursor(prepared = True);
+    cursor.execute(query,("%" + title + "%"));
+    res = cursor.fetchall();
+    return res; 
+#search by rating
+def findBookByRating(conn,low = 0,high = 5):
+    query = """
+    select generalInfo.dbNumber,title,lang,publisher,length,releaseDate,avgRating,price
+    from  generalInfo 
+    left join formats ON formats.dbNumber = generalInfo.dbNumber 
+    left join ratingsTotal on formats.versionNumber = ratingsTotal.versionNumber and ratingsTotal.dbNumber = formats.dbNumber
+    left join pricing on formats.versionNumber = pricing.versionNumber AND pricing.dbNumber = formats.dbNumber WHERE avgRating >= %s
+    AND avgRating <= %s ORDER BY avgRating DESC
+    """;
+    if (low > 5):
+        raise KeyError("Low cannot be that high");
+    elif (high < 1):
+        raise KeyError("High cannot be that low");
+    cursor = conn.cursor(prepared = True);
+    cursor.execute(query,(str(low),str(high)))
+    res = cursor.fetchall();
+    return res;
+    
+
